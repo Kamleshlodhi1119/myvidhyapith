da603af0bb0013494764ea193dacf510f89f77c4
KAMLESH LODHI <kamleshlodhi9302@gmail.com>
2023-01-20 18:22:37 +0530

    coming soon

---
+++ k3.js
@@ -0,0 +1,10 @@
+//alert("Created with love and the owner is mr. kamlesh");
+
+// open sidebar
+function toggleS() {
+    document.getElementById("sidebar").classList.toggle('active');
+}
+//time button
+function times() {
+    document.getElementById("btn").classList.toggle('fa-times');
+}
